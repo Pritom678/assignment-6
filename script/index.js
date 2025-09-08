9f0e6f81379b4b80b3131a0a4bc347061a9765b9
Pritom678 <152401917+Pritom678@users.noreply.github.com>
2025-09-08 13:25:38 +0600

    fourth commit

--- script/index.js
+++ script/index.js
@@ -21,8 +21,32 @@ const loadCategory = () => {
     })
 }
 
+//static li
+
+const staticLi = document.createElement('li')
+
+allCategories.innerHTML = '';
+staticLi.id = "all";
+staticLi.textContent = "All Trees";
+staticLi.className = "pl-2 font-semibold hover:bg-[#15803D] hover:border border-green-500 hover:text-white";
+allCategories.appendChild(staticLi)
+
+//showing all cards
+
+staticLi.addEventListener('click', () => {
+    plantsByCategory.innerHTML = '';
+    fetch('https://openapi.programming-hero.com/api/plants')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+})
+
+
+
+
 const showCategory = (categories) => {
-    allCategories.innerHTML = '';
+    
     categories.forEach(cat => {
         allCategories.innerHTML += `
         <li id="${cat.id}" class="pl-2 font-semibold hover:bg-[#15803D] hover:border border-green-500 hover:text-white">${cat.category_name}</li>
@@ -45,6 +69,9 @@ const showCategory = (categories) => {
     })
 }
 
+
+
+
 //all plants by categories
 const plantsByCategory = getElement('plant-container')
 
@@ -52,12 +79,14 @@ const loadPlantsByCategory = (categoryId) => {
     fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
     .then(res => res.json())
     .then(data => {
+
         showPlantByCategory(data.plants)
     })
     .catch(err => {
         console.log(err)
     })
 }
+// modal
 const loadPlantDetail = (id) =>{
     const url = `https://openapi.programming-hero.com/api/plant/${id}`;
     fetch(url)
@@ -68,7 +97,7 @@ const loadPlantDetail = (id) =>{
 }
 
 const showPlantDetail = (word) =>{
-    console.log(word)
+    // console.log(word)
     const detailBox = getElement('details-container');
     detailBox.innerHTML= `
                     <img src="${word.image}" class="rounded-xl h-30 w-full object-cover" alt="">
@@ -80,16 +109,17 @@ const showPlantDetail = (word) =>{
 
 
 const showPlantByCategory = (plants) =>{
+    console.log(plants)
     plantsByCategory.innerHTML = '';
     plants.forEach(plant => {
         plantsByCategory.innerHTML += `
-                    <div onclick="loadPlantDetail(${plant.id})" class="card bg-base-100 w-full shadow-lg p-2">
-                    <figure class="p-3">
+                    <div id="${plant.id}"  class="card bg-base-100 w-full shadow-lg p-2">
+                    <figure class="p-3" onclick="loadPlantDetail(${plant.id})">
                         <img
                         src="${plant.image}"
-                        class="rounded-xl h-30 w-full object-cover" />
+                        class="rounded-xl h-32 w-full object-cover" />
                     </figure>
-                    <div class="card-body items-center text-center">
+                    <div onclick="loadPlantDetail(${plant.id})" class="card-body items-center text-center">
                         <h2 class="card-title">${plant.name}</h2>
                         <p class="text-xs">${plant.description}</p>
                         <div class ="flex items-center gap-10">
@@ -98,13 +128,66 @@ const showPlantByCategory = (plants) =>{
                         </div>
                     </div>
                     <div class="card-actions">
-                        <button class="btn bg-[#15803D] text-white w-full rounded-full">Add to Card</button>
-                        </div>
+                        <button id="update-cart" class="btn bg-[#15803D] text-white w-full rounded-full">Add to Card</button>
+                    </div>
                     </div>
         `;
     });
 }
 
+//cart details
+let carts = []
+
+const cartContainer = getElement('cart-container');
+
+plantsByCategory.addEventListener('click', (e)=>{
+    if(e.target.innerText === 'Add to Card'){
+        
+        handleCart(e);
+    }
+})
+ const handleCart = (e) => {
+    const title = e.target.parentNode.parentNode.children[1].children[0].innerText;
+        const price = e.target.parentNode.parentNode.children[1].children[2].children[1].innerText;
+        const id = e.target.parentNode.parentNode.id;
+        
+        const existingItems = carts.filter(cart => cart.id === id);
+        if(existingItems.length === 0){
+            carts.push({
+            title: title,
+            price: price,
+            id: id
+        })
+        showCarts(carts)
+        }
+
+        
+ }
+
+const showCarts = (carts) => {
+    cartContainer.innerHTML='';
+    carts.forEach(cart => {
+        cartContainer.innerHTML += `
+        <div class="flex justify-between items-center p-3 rounded-lg bg-[#DCFCE7] my-3">
+                        <div >
+                            <h1 class="text-lg font-semibold">${cart.title}</h1>
+                            <p class="text-sm font-semibold">${cart.price}</p>
+                        </div>
+                        <div>
+                            <i onclick = "handleDeleteCart(${cart.id})" class="fa-solid fa-xmark"></i>
+                        </div>
+                    </div>
+        `
+    });
+}
+//problem not solved
+const handleDeleteCart = (cartId) => {
+    const filterCarts = carts.filter(cart =>  cart.id != cartId)
+    
+    carts = filterCarts;
+    // console.log(carts)
+    showCarts(carts)
+} 
 
 loadCategory();
-loadPlantsByCategory();
+// loadPlantsByCategory();
