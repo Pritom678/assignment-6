f85bbae4ef5f574cb62bd68542ec85b5a460e324
Pritom678 <152401917+Pritom678@users.noreply.github.com>
2025-09-08 18:42:19 +0600

    fifth commit

--- script/index.js
+++ script/index.js
@@ -35,26 +35,7 @@ const manageSpinner = (status) =>{
 }
 
 
-//static li
 
-const staticLi = document.createElement('li')
-
-allCategories.innerHTML = '';
-staticLi.id = "all";
-staticLi.textContent = "All Trees";
-staticLi.className = "pl-2 font-semibold hover:bg-[#15803D] hover:border border-green-500 hover:text-white";
-allCategories.appendChild(staticLi)
-
-//showing all cards
-
-staticLi.addEventListener('click', () => {
-    plantsByCategory.innerHTML = '';
-    fetch('https://openapi.programming-hero.com/api/plants')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-})
 
 
 
@@ -95,7 +76,7 @@ const loadPlantsByCategory = (categoryId) => {
     .then(res => res.json())
     .then(data => {
 
-        showPlantByCategory(data.plants)
+        showLoadPlantsByCategory(data.plants)
     })
     .catch(err => {
         console.log(err)
@@ -123,33 +104,75 @@ const showPlantDetail = (word) =>{
 }
 
 
-const showPlantByCategory = (plants) =>{
-    console.log(plants)
-    plantsByCategory.innerHTML = '';
-    plants.forEach(plant => {
+const showLoadPlantsByCategory = (trees) => {
+    // console.log(trees)
+    plantsByCategory.innerHTML = ''
+    trees.forEach(tree => {
         plantsByCategory.innerHTML += `
-                    <div id="${plant.id}"  class="card bg-base-100 w-full shadow-lg p-2">
-                    <figure class="p-3" onclick="loadPlantDetail(${plant.id})">
-                        <img
-                        src="${plant.image}"
-                        class="rounded-xl h-32 w-full object-cover" />
-                    </figure>
-                    <div onclick="loadPlantDetail(${plant.id})" class="card-body items-center text-center">
-                        <h2 class="card-title">${plant.name}</h2>
-                        <p class="text-xs">${plant.description}</p>
-                        <div class ="flex items-center gap-10">
-                            <h1 class="text-sm text-[#15803D] bg-[#DCFCE7] rounded-full p-2 border">${plant.category}</h1>
-                            <h1 class="font-bold">৳${plant.price}</h1>
+                    
+                        <div id="${tree.id}" class="card bg-base-100 shadow-sm p-3">
+                        <figure onclick ="loadPlantDetail(${tree.id})" class="p-2">
+                            <img
+                            src="${tree.image}"
+                            class="rounded-xl h-32 w-full object-cover" />
+                        </figure>
+                        <div onclick ="loadPlantDetail(${tree.id})" class="card-body items-center text-center">
+                            <h2 class="card-title">${tree.name}</h2>
+                            <p>${tree.description}</p>
                         </div>
-                    </div>
-                    <div class="card-actions">
-                        <button id="update-cart" class="btn bg-[#15803D] text-white w-full rounded-full">Add to Card</button>
-                    </div>
-                    </div>
-        `;
+                        <div onclick ="loadPlantDetail(${tree.id})" class="flex justify-between items-center p-5">
+                            <h1 class="text-[#15803D] bg-[#DCFCE7] rounded-full p-2 font-semibold">${tree.category}</h1>
+                            <h1 class="font-semibold">৳${tree.price}</h1>
+                        </div>
+                            <div class="card-actions">
+                                <button class="btn w-full bg-[#15803D] text-white">Add to Card</button>
+                            </div>
+                        </div>
+                    
+        `
     });
     manageSpinner(false)
 }
+
+
+const loadAllPlants = () =>{
+    manageSpinner(true)
+    fetch('https://openapi.programming-hero.com/api/plants')
+    .then(res => res.json())
+    .then(data => {
+        showLoadAllPlants(data.plants)
+    })
+}
+
+const showLoadAllPlants = (trees) =>{
+    plantsByCategory.innerHTML = ''
+    trees.forEach(tree => {
+        plantsByCategory.innerHTML += `
+                    
+                        <div id="${tree.id}" class="card bg-base-100 shadow-sm p-3">
+                        <figure onclick ="loadPlantDetail(${tree.id})" class="p-2">
+                            <img
+                            src="${tree.image}"
+                            class="rounded-xl h-32 w-full object-cover" />
+                        </figure>
+                        <div onclick ="loadPlantDetail(${tree.id})" class="card-body items-center text-center">
+                            <h2 class="card-title">${tree.name}</h2>
+                            <p>${tree.description}</p>
+                        </div>
+                        <div onclick ="loadPlantDetail(${tree.id})" class="flex justify-between items-center p-5">
+                            <h1 class="text-[#15803D] bg-[#DCFCE7] rounded-full p-2 font-semibold">${tree.category}</h1>
+                            <h1 class="font-semibold">৳${tree.price}</h1>
+                        </div>
+                            <div class="card-actions">
+                                <button class="btn w-full bg-[#15803D] text-white">Add to Card</button>
+                            </div>
+                        </div>
+                    
+        `
+    });
+    manageSpinner(false)
+}
+
 
 //cart details
 let carts = []
@@ -164,8 +187,8 @@ plantsByCategory.addEventListener('click', (e)=>{
     }
 })
  const handleCart = (e) => {
-    const title = e.target.parentNode.parentNode.children[1].children[0].innerText;
-        const price = e.target.parentNode.parentNode.children[1].children[2].children[1].innerText;
+        const title = e.target.parentNode.parentNode.children[1].children[0].innerText;
+        const price = e.target.parentNode.parentNode.children[2].children[1].innerText;
         const id = e.target.parentNode.parentNode.id;
         
         const existingItems = carts.filter(cart => cart.id === id);
@@ -203,7 +226,7 @@ const showCarts = (carts) => {
     }, 0)
     cartTotal.textContent = `Total: ৳${total}`;
 }
-//problem not solved
+
 const handleDeleteCart = (cartId) => {
     const filterCarts = carts.filter(cart =>  cart.id != cartId)
     
@@ -213,4 +236,4 @@ const handleDeleteCart = (cartId) => {
 } 
 
 loadCategory();
-// loadPlantsByCategory();
+loadAllPlants();
